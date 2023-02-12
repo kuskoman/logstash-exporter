ba21baa19137c309c9dd3af67ece229288f37eb1
Jakub <kubasurdej@gmail.com>
2023-02-12 11:39:50 +0100

    Create test for collector manager

---
+++ collectors/collector_manager_test.go
@@ -0,0 +1,38 @@
+package collectors
+
+import (
+	"testing"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type mockCollector struct{}
+
+func (m *mockCollector) Collect(ch chan<- prometheus.Metric) error {
+	ch <- prometheus.MustNewConstMetric(
+		prometheus.NewDesc("mock_metric", "mock metric description", nil, nil),
+		prometheus.GaugeValue,
+		1,
+	)
+	return nil
+}
+
+func TestCollect(t *testing.T) {
+	cm := &CollectorManager{
+		collectors: map[string]Collector{
+			"mock": &mockCollector{},
+		},
+		scrapeDurations: getScrapeDurationsCollector(),
+	}
+
+	ch := make(chan prometheus.Metric)
+	go cm.Collect(ch)
+
+	metric := <-ch
+
+	desc := metric.Desc()
+	expectedDesc := "Desc{fqName: \"mock_metric\", help: \"mock metric description\", constLabels: {}, variableLabels: []}"
+	if desc.String() != expectedDesc {
+		t.Errorf("Expected metric description to be '%s', got %s", expectedDesc, desc.String())
+	}
+}
