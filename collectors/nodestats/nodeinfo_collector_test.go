da821ed7318baa8dbe2a222712dfe60e74f10d7e
Jakub <kubasurdej@gmail.com>
2023-02-12 11:54:51 +0100

    Add simple test for nodeinfo collector

--- collectors/nodestats/nodeinfo_collector_test.go
+++ collectors/nodestats/nodeinfo_collector_test.go
@@ -3,14 +3,16 @@ package nodestats
 import (
 	"encoding/json"
 	"io/ioutil"
+	"testing"
 
 	"github.com/kuskoman/logstash-exporter/fetcher/responses"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 type mockClient struct{}
 
 func (m *mockClient) GetNodeInfo() (*responses.NodeInfoResponse, error) {
-	b, err := ioutil.ReadFile("node_info.json")
+	b, err := ioutil.ReadFile("../../fixtures/node_info.json")
 	if err != nil {
 		return nil, err
 	}
@@ -22,4 +24,17 @@ func (m *mockClient) GetNodeInfo() (*responses.NodeInfoResponse, error) {
 	}
 
 	return &nodeInfo, nil
+}
+
+func TestCollectNotNil(t *testing.T) {
+	collector := NewNodestatsCollector(&mockClient{})
+	ch := make(chan prometheus.Metric)
+	go collector.Collect(ch)
+
+	for i := 0; i < 6; i++ {
+		metric := <-ch
+		if metric == nil {
+			t.Errorf("Expected metric %s to be not nil", metric.Desc().String())
+		}
+	}
 }
