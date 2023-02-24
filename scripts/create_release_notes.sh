b9727b1061110893e6042fb43ca11095fbbf7124
Jakub <kubasurdej@gmail.com>
2023-02-24 09:35:59 +0100

    Remove merge commits from release notes

--- scripts/create_release_notes.sh
+++ scripts/create_release_notes.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-# Get the latest tag
 last_tag=$(git describe --abbrev=0 --tags)
 
-# Get the commit messages since the last tag
-commits=$(git log $last_tag..HEAD --pretty=format:"%s")
+commits_since_last_tag=$(git log $last_tag..HEAD --no-merges --pretty=format:"%s")
 
 notes_file="release_notes.txt"
 
-if [ -z "$commits" ]; then
+if [ -z "$commits_since_last_tag" ]; then
   echo "No changes from previous release" >> "$notes_file"
 else
-  # Write the release notes to a file
   echo "Release Notes:" > "$notes_file"
   echo "" >> "$notes_file"
   echo "Since the last tag ($last_tag), the following changes have been made:" >> "$notes_file"
   echo "" >> "$notes_file"
-  echo "$commits" >> "$notes_file"
+  echo "$commits_since_last_tag" >> "$notes_file"
 fi
