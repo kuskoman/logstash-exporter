2374fd18c809712f34ee44d2c828be97ce384e40
Jakub <kubasurdej@gmail.com>
2023-03-05 08:29:45 +0100

    Improve extracting collectors for nodestats in test

--- helpers/prometheus_helper_test.go
+++ helpers/prometheus_helper_test.go
@@ -1,6 +1,7 @@
 package helpers
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -14,5 +15,23 @@ func TestSimpleDescHelper(t *testing.T) {
 	expectedDesc := "Desc{fqName: \"logstash_exporter_test_metric\", help: \"metric\", constLabels: {}, variableLabels: []}"
 	if desc.String() != expectedDesc {
 		t.Errorf("incorrect metric description, expected %s but got %s", expectedDesc, desc.String())
+	}
+}
+
+func TestExtractFqdnName(t *testing.T) {
+	helper := &SimpleDescHelper{
+		Namespace: "logstash_exporter",
+		Subsystem: "test",
+	}
+
+	metricSubname := "fqdn_metric"
+	desc := helper.NewDesc(metricSubname).String()
+	fqdn, err := ExtractFqName(desc)
+	if err != nil {
+		t.Errorf("failed to extract fqName from metric %s", desc)
+	}
+
+	if fqdn != fmt.Sprintf("logstash_exporter_test_%s", metricSubname) {
+		t.Errorf("incorrect fqdn, expected %s but got %s", "logstash_exporter_test_"+metricSubname, fqdn)
 	}
 }
