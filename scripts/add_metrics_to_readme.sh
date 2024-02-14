184fb0d3ef3ea04a2e1a8ee7bf4c506097720381
satk0 <satko_a@op.pl>
2024-02-14 12:13:30 +0100

    Correct lint suggestions for  script (issue #269)

--- scripts/add_metrics_to_readme.sh
+++ scripts/add_metrics_to_readme.sh
@@ -15,15 +15,15 @@ while IFS= read -r line; do LINES+=("$line"); done < $FILE
 startLine=$(grep -n "^<!-- METRICS_TABLE_START -->" $FILE | awk -F: '{print $1}')
 endLine=$(grep -n "^<!-- METRICS_TABLE_END -->" $FILE | awk -F: '{print $1}')
 
-metricsTable=$(echo "| Name | Type | Description |
+metricsTable="| Name | Type | Description |
 | ----------- | ----------- | ----------- |
-$(getMetrics | sort --version-sort)")
+$(getMetrics | sort --version-sort)"
 
 for ((i=0; i<${#LINES[@]}; i++)); do
-    if [ $i -eq $startLine ]; then
+    if [ $i -eq "$startLine" ]; then
         echo -e "${LINES[i]}"
         echo "$metricsTable"
-    elif [ $i -gt $startLine ] && [ $i -lt $((endLine-2)) ]; then # -2 because of the empty line before the end marker
+    elif [ $i -gt "$startLine" ] && [ $i -lt $((endLine-2)) ]; then # -2 because of the empty line before the end marker
         continue
     else
         echo -e "${LINES[i]}"
