f1caab839d8b723cd0fb7eb123a53f8c3b1aba51
satk0 <54475808+satk0@users.noreply.github.com>
2024-02-16 11:29:19 +0000

    Fix 'verify_metrics.sh' (closes #271) (#288)

--- scripts/verify_metrics.sh
+++ scripts/verify_metrics.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/env bash
 
 prometheus_response=$(curl -s -X GET http://localhost:9090/api/v1/label/__name__/values)
-status=$(echo $prometheus_response | jq -r '.status')
+status=$(echo "$prometheus_response" | jq -r '.status')
 if [ "$status" != "success" ]; then
     echo "Prometheus API returned an error: $prometheus_response"
     exit 1
 fi
 
-logstash_metrics=$(echo $prometheus_response | jq -r '.data[]' | grep -E '^logstash_' | sort)
+logstash_metrics=$(echo "$prometheus_response" | jq -r '.data[]' | grep -E '^logstash_' | sort)
 script_location=$(dirname "$0")
 snapshot_dir="$script_location/snapshots"
 snapshot_file="$snapshot_dir/metric_names.txt"
@@ -24,7 +24,7 @@ snapshot_metrics=$(cat "$snapshot_file")
 echo "Checking that all metrics are in the snapshot file $snapshot_file"
 for metric in $logstash_metrics; do
     echo "Checking existence of metric $metric"
-    if [[ ! "$snapshot_metrics" =~ "$metric" ]]; then
+    if [[ ! "$snapshot_metrics" =~ $metric ]]; then
         if [ "$CI" == "true" ]; then
             echo "Metric $metric is not in the snapshot file $snapshot_file"
             exit 1
@@ -37,8 +37,8 @@ done
 
 for metric in $logstash_metrics; do
     echo "Checking metric endpoint for $metric"
-    prometheus_response=$(curl -s -X GET http://localhost:9090/api/v1/series?match[]=$metric)
-    status=$(echo $prometheus_response | jq -r '.status')
+    prometheus_response=$(curl -s -X GET http://localhost:9090/api/v1/series?match[]="$metric")
+    status=$(echo "$prometheus_response" | jq -r '.status')
     if [ "$status" != "success" ]; then
         echo "Prometheus API returned an error: $prometheus_response"
         exit 1
